70ac033717ba1bfab8ed0beb6ec58c6e70d928d6
1dawu2 <david_wurm@web.de>
2021-12-02 21:38:54 +0100

    Init 0.1

--- tf-sac.js
+++ tf-sac.js
@@ -441,7 +441,7 @@
                                             var result = oModel.getData();
                                             console.log(result);
 
-                                            _result = result_final;
+                                            _result = result;
 
                                             that._firePropertiesChanged();
                                             this.settings = {};
